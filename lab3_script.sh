737a6430c602516c26e26ba706f9d4e5d139059a
Jacob Felknor <jacobfelknor073@gmail.com>
2019-09-20 09:18:47 -0600

    changed lab3_script to only output 303 #s

--- lab3_script.sh
+++ lab3_script.sh
@@ -18,6 +18,6 @@ grep -c -P [a-zA-Z0-9_]+@[a-zA-Z0-9_]+\.[a-zA-Z] regex_practice.txt
 grep -o -P [a-zA-Z0-9_]+@[a-zA-Z0-9_]+\.[a-zA-Z] regex_practice.txt >> email_results.txt
 
 # 303 phone numbers
-# grep -c -P 303[-./]?[0-9]{3}[-./]?[0-9]{4} regex_practice.txt
+grep -o -P 303[-./]?[0-9]{3}[-./]?[0-9]{4} regex_practice.txt
 # all phone numbers
-grep -o -P [0-9]{3}[-./][0-9]{3}[-./][0-9]{4} regex_practice.txt
+# grep -o -P [0-9]{3}[-./][0-9]{3}[-./][0-9]{4} regex_practice.txt
